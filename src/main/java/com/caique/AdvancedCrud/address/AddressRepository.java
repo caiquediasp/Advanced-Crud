d70169fda68e30a22973bebb366e605666b53c00
caiquediasp <caiquediasp@gmail.com>
2026-06-23 22:32:26 -0300

    feat: user and address repositories

---
+++ src/main/java/com/caique/AdvancedCrud/address/AddressRepository.java
@@ -0,0 +1,20 @@
+package com.caique.AdvancedCrud.address;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    Page<Address> findByUserId(Long userId, Pageable pageable);
+
+    Optional<Address> findByPublicIdAndUserId(UUID publicId, Long userId);
+
+    Optional<Address> findByUserIdAndPrimaryIsTrue(Long userId);
+
+}
