f7571e8cc6639e4ae19a04b0e104f7575c21fe3b
caiquediasp <caiquediasp@gmail.com>
2026-07-14 10:19:52 -0300

    test: error flow using rabbitMQ test

---
+++ src/test/java/com/caique/AdvancedCrud/shared/errorLog/CriticalErrorTestController.java
@@ -0,0 +1,13 @@
+package com.caique.AdvancedCrud.shared.errorLog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CriticalErrorTestController {
+
+    @GetMapping("/api/v1/test/critical-error")
+    public void throwCriticalError() {
+        throw new IllegalStateException("Forced critical error for integration test");
+    }
+}
